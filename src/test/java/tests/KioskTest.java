a69bd1a045b0a2a8386388e43727340e441aaaa4
mslp5 <mslp561@gmail.com>
2022-11-25 00:51:46 +0300

    Убрал лишние импорты и эксепшны

--- src/test/java/tests/KioskTest.java
+++ src/test/java/tests/KioskTest.java
@@ -15,7 +15,7 @@ public class KioskTest extends BaseTest {
     private WebDriverWait wait;
 
     @Test
-    public void kioskTest () throws InterruptedException {
+    public void kioskTest () {
         startUp();
         driver.manage().window().fullscreen(); //Запуск в режиме "Киоск"
         driver.get("https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public abstract class BaseTest {
     public WebDriver driver;
